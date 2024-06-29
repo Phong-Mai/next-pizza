5c8ba8275c27520b10ab9e47ac0677ae76bddedc
Phong Mai <113613135+Phong-Mai@users.noreply.github.com>
2024-06-29 15:11:46 +0700

    caaaaa

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,2 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_BASE_URL
-    : "http://localhost:3000/";
+export const baseUrl = "https://next-pizza-nu.vercel.app/"
+
